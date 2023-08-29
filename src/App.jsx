2149091d75b42909eb14af55da0716d14093f82b
NekoJar <mzharfanhakim7@gmail.com>
2023-08-29 18:13:53 +0700

    implementing the countries list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -7,7 +9,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import Form from "./components/Form";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -46,7 +48,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />} />
